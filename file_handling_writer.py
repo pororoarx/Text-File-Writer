d7e39daac7d947211a5c0f387c4ed13a87587f90
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-27 11:17:14 +0800

    Pseudocode

--- file_handling_writer.py
+++ file_handling_writer.py
@@ -1,1 +1,12 @@
 # Write a method in python to write multiple line of text contents into a text file mylife.txt. 
+
+# Start
+# Create a function called process
+    # Open the mylife.txt (write)
+        # Ask the user to enter an input string
+            # Write the line to the file
+            # Ask the ser if they want to input string again
+            # If user enters 'n', exit the loop
+
+
+# execute the function
